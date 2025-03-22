a790e086d0012fd0892858ea70b681b9263ea20f
Argeu Venturini <argeu.v@hotmail.com>
2025-03-22 13:00:27 -0300

    Adding orders post method to backend

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -40,26 +40,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      Provider.of<OrderList>(
-                        context,
-                        listen: false,
-                      ).addOrder(cart);
-
-                      cart.clear();
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.secondary,
-                    ),
-                    child: Text(
-                      'Finalizar a Compra',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
+                  CartButton(cart: cart),
                 ],
               ),
             ),
@@ -74,5 +55,45 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({super.key, required this.cart});
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? CircularProgressIndicator()
+        : TextButton(
+          onPressed:
+              widget.cart.itemsCount == 0
+                  ? null
+                  : () async {
+                    setState(() => _isLoading = true);
+                    await Provider.of<OrderList>(
+                      context,
+                      listen: false,
+                    ).addOrder(widget.cart);
+                    widget.cart.clear();
+                    setState(() => _isLoading = false);
+                  },
+          style: TextButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+          ),
+          child: Text(
+            'Finalizar a Compra',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,4 @@
 class Constants {
   static const productBaseUrl = 'https://shop-cod3r-b21d3-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl = 'https://shop-cod3r-b21d3-default-rtdb.firebaseio.com/orders';
 }
