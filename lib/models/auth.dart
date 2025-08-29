6be8d01510520e56fca97038f793d305b9f8b003
Argeu Venturini <argeu.v@hotmail.com>
2025-08-29 17:59:46 -0300

    Implementação persistencia de Autenticação na memória usando shared_preferences

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/data/store.dart';
 import 'package:shop_app/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
@@ -56,6 +57,14 @@ class Auth with ChangeNotifier {
       _expiresDate = DateTime.now().add(
         Duration(seconds: int.parse(body['expiresIn'])),
       );
+
+      Store.saveMap('userData', {
+        'token': _token,
+        'email': _email,
+        'userId': _userId,
+        'expiresDate': _expiresDate!.toIso8601String(),
+      });
+
       _autoLogout();
       notifyListeners();
     }
@@ -69,13 +78,32 @@ class Auth with ChangeNotifier {
     return _authenticate(email, password, 'signInWithPassword');
   }
 
+  Future<void> tryAutoLogin() async {
+    if (isAuth) return;
+
+    final userData = await Store.getMap('userData');
+    if (userData.isEmpty) return;
+
+    final expiresDate = DateTime.parse(userData['expiresDate']);
+    if (expiresDate.isBefore(DateTime.now())) return;
+
+    _token = userData['token'];
+    _email = userData['email'];
+    _userId = userData['userId'];
+    _expiresDate = expiresDate;
+
+    _autoLogout();
+    notifyListeners();
+
+  }
+
   void logout() {
     _token = null;
     _email = null;
     _userId = null;
     _expiresDate = null;
     _clearLogoutTimer();
-    notifyListeners();
+    Store.remove('userData').then((_) => notifyListeners());
   }
 
   void _clearLogoutTimer() {
